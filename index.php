b5f46a576eb12e66f3f2c9fae6333544968f2410
Nathaelle <scriptales.clients@gmail.com>
2020-03-23 11:15:24 +0100

    Correction exercice 4 : 2nde PARTIE

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-    var_dump($_GET);
+    //var_dump($_GET);
 
     $page = isset($_GET["page"])? $_GET["page"] : "home";
 
     switch ($page) {
         case "home":
-            echo "Vous avez demandé la page d'accueil !";
+            $include = "recettes/home.html";
             break;
         case "entree":
-            echo "Vous avez demandé la page entrées !";
+            $include = "recettes/entree.html";
             break;
         case "plat":
-            echo "Vous avez demandé la page plats !";
+            $include = "recettes/plat.html";
             break;
         case "dessert":
-            echo "Vous avez demandé la page desserts !";
+            $include = "recettes/dessert.html";
             break;
-        default : echo "Vous avez demandé la page d'accueil !";
+        default : $include = "recettes/home.html";
     }
 ?>
 
@@ -33,15 +33,11 @@
         <header>
             <?php require "html/menu.php" ?>
         </header>
+
+        <section class="content">
+            <?php require $include ?>
+        </section>
         
-        <article>
-            <h2 class="text-center">Suggestion du jour</h2>
-            <ul class="row items">
-                <li class="img-bullet col-4 text-center"><img src="img/entree3.jpg" alt="Photo de l'entrée 3"><br><a href ="recettes/entrees/entree3.html">Entrée 3</a></li>
-                <li class="img-bullet col-4 text-center"><img src="img/plat1.jpg" alt="Photo du plat 1"><br><a href ="recettes/plats/plat1.html">Plat 1</a></li>
-                <li class="img-bullet col-4 text-center"><img src="img/dessert1.jpg" alt="Photo du dessert 1"><br><a href ="recettes/desserts/dessert1.html">Dessert 1</a></li>
-            </ul>
-        </article>
     </body>
 
 </html>
