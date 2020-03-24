453d90573afbb08bb0078eb1a7f448f1ccd5d9d2
Nathaelle <scriptales.clients@gmail.com>
2020-03-24 08:39:42 +0100

    Correction PARTIE 2 exercice 1

---
+++ recettes.php
@@ -0,0 +1,12 @@
+<?php
+
+$recette_test = [
+    "titre" => "Entree 1",
+    "image" => "entree1.jpg",
+    "temps" => "15sec",
+    "personnes" => 8,
+    "difficulte" => 1,
+    "cout" => 1,
+    "ingredients" => "1 paquet de pâtes de lasagnes///3 oignons jaunes///3 gousses d'ail///1 branche de céleri///1 carotte</li>",
+    "etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
+];
