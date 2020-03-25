666b8f76950ac1ff59aa61d40a0d2813f6e08528
Nathaelle <scriptales.clients@gmail.com>
2020-03-25 08:20:39 +0100

    Correction exercice 3 partie 2

--- recettes.php
+++ recettes.php
@@ -1,12 +1,42 @@
 <?php
 
-$recette_test = [
-    "titre" => "Entree 1",
+$recette1 = [
+    "titre" => "Entrée 1",
     "image" => "entree1.jpg",
+    "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, voluptatem.",
     "temps" => "15sec",
     "personnes" => 8,
     "difficulte" => 1,
     "cout" => 1,
-    "ingredients" => "1 paquet de pâtes de lasagnes///3 oignons jaunes///3 gousses d'ail///1 branche de céleri///1 carotte</li>",
+    "ingredients" => "1 paquet de pâtes de lasagnes///3 oignons jaunes///3 gousses d'ail///1 branche de céleri///1 carotte",
     "etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
+];
+
+$recette2 = [
+    "titre" => "Entrée 2",
+    "image" => "entree2.jpg",
+    "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, voluptatem.",
+    "temps" => "15sec",
+    "personnes" => 8,
+    "difficulte" => 1,
+    "cout" => 1,
+    "ingredients" => "1 paquet de pâtes de lasagnes///3 oignons jaunes///3 gousses d'ail///1 branche de céleri///1 carotte",
+    "etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
+];
+
+$recette3 = [
+    "titre" => "Entrée 3",
+    "image" => "entree3.jpg",
+    "description" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa, voluptatem.",
+    "temps" => "15sec",
+    "personnes" => 8,
+    "difficulte" => 1,
+    "cout" => 1,
+    "ingredients" => "1 paquet de pâtes de lasagnes///3 oignons jaunes///3 gousses d'ail///1 branche de céleri///1 carotte",
+    "etapes" => "Faire revenir gousses hachées d'ail et les oignons émincés dans un peu d'huile d'olive.///Ajouter la carotte et la branche de céleri hachée puis la viande et faire revenir le tout.///Au bout de quelques minutes, ajouter le vin rouge. Laisser cuire jusqu'à évaporation."
+];
+
+$cat_test = [
+    "nom" => "Les entrées",
+    "recettes" => [$recette1, $recette2, $recette3]
 ];
